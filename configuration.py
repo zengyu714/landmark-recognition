aca73ddf03bbb938b0507ce83d7d96e1618cb1e8
Chaonan Ye <canonyeee@gmail.com>
2019-05-31 02:18:52 -0700

    change max

--- configuration.py
+++ configuration.py
@@ -16,8 +16,8 @@ CONF.training_toy_dataset = False
 # 4132914 = 37123 (train) + 247975 (val) + 165316 (test)
 CONF.data_split = {'train': 0.9, 'val': 0.06, 'test': 0.04}
 CONF.image_size = (224, 224)
-CONF.min_samples = 200
-CONF.max_samples = 500
+CONF.min_samples = 183
+CONF.max_samples = 10000
 # filtered with 18k classes
 
 # any positive number to slice the training loader
