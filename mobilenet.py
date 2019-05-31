a25cf4df070c8a589315ec3c18f761251b228bdb
Chaonan Ye <canonyeee@gmail.com>
2019-05-31 01:35:05 -0700

    squeezenet

--- mobilenet.py
+++ mobilenet.py
@@ -1,5 +1,8 @@
 from torch import nn
-from .utils import load_state_dict_from_url
+try:
+    from torch.hub import load_state_dict_from_url
+except ImportError:
+    from torch.utils.model_zoo import load_url as load_state_dict_from_url
 
 
 __all__ = ['MobileNetV2', 'mobilenet_v2']
